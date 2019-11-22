f2c6160eaa289db2ebbe6832a1e0d4b8218f97aa
acse-mgs219 <michel.sfeir19@imperial.ac.uk>
2019-11-22 14:35:48 +0000

    Added database files

---
+++ NoSQL Project/Login.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Login
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles signUpBtn.Click
+        SignUp.Show();
+        Me.Hide()
+
+    End Sub
+
+    Private Sub cancelBtn_Click(sender As Object, e As EventArgs) Handles cancelBtn.Click
+        Me.Close()
+    End Sub
+
+    Private Sub signInBtn_Click(sender As Object, e As EventArgs) Handles signInBtn.Click
+        Dim cmd As SqlCommand = New SqlCommand("SELECT * FROM users WHERE username = '" & TextBox1.Text & "' AND password = '" & TextBox2.Text & "' ", connection.con)
+        connection.con.Open()
+        Dim dr As SqlDataReader = cmd.ExecuteReader()
+        If (dr.Read() = True) Then
+            Main.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Invalid Username or Password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+    End Sub
+End Class
